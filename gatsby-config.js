eb1860674a62b3514b671dd54a3e1f051cc8d693
Seth Hall <seth@ushahidi.com>
2020-02-18 13:50:49 -0500

    trying to fix login via Netlify Identity

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `David K. Stanley Studio`,
+    author: `David Stanley`,
+    description: `The home of DKS Studio`,
+    siteUrl: `https://dks-studio.netlify.com/`,
     social: {
       twitter: `kylemathews`,
     },
