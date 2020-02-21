a1ae9df6427a29cddaa849b6e7d2df8da9d78451
Seth Hall <seth@ushahidi.com>
2020-02-20 23:57:56 -0500

    changes content directory in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content`,
+        path: `${__dirname}/src/content`,
         name: `content`,
       },
     },
