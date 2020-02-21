d955b1217064a0ca9473aaa9a0c51b51cf46be3f
Seth Hall <seth@ushahidi.com>
2020-02-20 21:34:47 -0500

    trying to add a new collection to CMS admin UI

--- src/styles/StyledPortfolioGridItem.js
+++ src/styles/StyledPortfolioGridItem.js
@@ -1,30 +1,37 @@
 import styled from "styled-components"
 import { device } from "./MediaQueries"
 
+// import { rhythm, scale } from "../utils/typography"
+
 export const StyledPortfolioGridItem = styled.div`
-  width: 100%;
-  margin-bottom: 30px;
+         width: 100%;
+         margin-bottom: 30px;
 
-  @media ${device.mobileUp} {
-    width: 48%;
-  }
+         @media ${device.mobileUp} {
+           width: 48%;
+         }
 
-  @media ${device.tabletUp} {
-    width: 31.3%;
-  }
+         @media ${device.tabletUp} {
+           width: 31.3%;
+         }
 
-  a {
-    box-shadow: none;
-    color: black;
-  }
+         a {
+           box-shadow: none;
+           color: black;
+         }
 
-  img {
-    width: 100%;
-    margin: 0 auto 5px;
-  }
+         img {
+           width: 100%;
+           margin: 0 auto 5px;
+         }
 
-  .portfolio-cat {
-    margin: 0;
-    text-align: center;
-  }
-`
+         .portfolio-cat {
+           margin: 0;
+           font-size: 12px;
+           text-align: center;
+
+           @media ${device.tabletUp} {
+             font-size: 14px;
+           }
+         }
+       `
