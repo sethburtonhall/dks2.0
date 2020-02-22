09f88037f7bd14e8a8b1dba1622a27248f2c03a7
Seth Hall <seth@ushahidi.com>
2020-02-22 16:22:37 -0500

    brings back about bio and contactInfo components with queries

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,67 +1,65 @@
 import React from "react"
-// import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 // Styles
 import { Container } from "../styles/GlobalStyles"
 
 const Bio = () => {
   return (
-    // <StaticQuery
-    //   query={bioQuery}
-    //   render={data => {
-    //     const { author, social } = data.site.siteMetadata
-    //     const { name, title, intro } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-    //     return (
-    //       <Container>
-    //         <img
-    //           src="/assets/david-stanley.jpg"
-    //           alt={author}
-    //         />
-    //         <h1 className="name">{name}</h1>
-    //         <h4 className="title">{title}</h4>
-    //         <div className="social">
-    //           <a href={`https://www.instagram.com/${social.instagram}`}>
-    //             Instagram
-    //           </a>
-    //           <a href={social.facebook}>Facebook</a>
-    //           <a href={social.linkedin}>LinkedIn</a>
-    //         </div>
-    //         <p>{intro}</p>
-    //       </Container>
-    //     )
-    //   }}
-    // />
-
-    <div>test</div>
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        const { name, title, intro } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+        return (
+          <Container>
+            <img
+              src="/assets/david-stanley.jpg"
+              alt={author}
+            />
+            <h1 className="name">{name}</h1>
+            <h4 className="title">{title}</h4>
+            <div className="social">
+              <a href={`https://www.instagram.com/${social.instagram}`}>
+                Instagram
+              </a>
+              <a href={social.facebook}>Facebook</a>
+              <a href={social.linkedin}>LinkedIn</a>
+            </div>
+            <p>{intro}</p>
+          </Container>
+        )
+      }}
+    />
   )
 }
 
-// const bioQuery = graphql`
-//   query BioQuery {
-//     site {
-//       siteMetadata {
-//         author
-//         social {
-//           facebook
-//           instagram
-//           linkedin
-//         }
-//       }
-//     }
-//     allFile(filter: { name: { eq: "about" } }) {
-//       edges {
-//         node {
-//           childMarkdownRemark {
-//             frontmatter {
-//               name
-//               title
-//               intro
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+const bioQuery = graphql`
+  query BioQuery {
+    site {
+      siteMetadata {
+        author
+        social {
+          facebook
+          instagram
+          linkedin
+        }
+      }
+    }
+    allFile(filter: { name: { eq: "about" } }) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              name
+              title
+              intro
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Bio
--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,45 +1,46 @@
 import React from "react"
-// import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 // Styles
 import { Container } from "../styles/GlobalStyles"
 
 const ContactInfo = () => {
   return (
-    // <StaticQuery
-    //   query={contactQuery}
-    //   render={data => {
-    //     const { email, phone } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-    //     return (
-    //       <Container>
-    //         <div className="contact">
-    //           <div>{email}</div>
-    //           <div>{phone}</div>
-    //         </div>
-    //       </Container>
-    //     )
-    //   }}
-    // />
-
-    <div>test</div>
+    <StaticQuery
+      query={contactQuery}
+      render={data => {
+        const {
+          email,
+          phone,
+        } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+        return (
+          <Container>
+            <div className="contact">
+              <div>{email}</div>
+              <div>{phone}</div>
+            </div>
+          </Container>
+        )
+      }}
+    />
   )
 }
 
-// const contactQuery = graphql`
-//   query ContactQuery {
-//     allFile(filter: { name: { eq: "contact" } }) {
-//       edges {
-//         node {
-//           childMarkdownRemark {
-//             frontmatter {
-//               email
-//               phone
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+const contactQuery = graphql`
+  query ContactQuery {
+    allFile(filter: { name: { eq: "contact" } }) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              email
+              phone
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ContactInfo
