05760c5ed32949326518d0155153d8a027cef47f
Seth Hall <seth@ushahidi.com>
2020-02-22 15:55:17 -0500

    trying to fix About and Contact data structure and GraphQL error

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,27 +1,27 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+// import React from "react"
+// import { StaticQuery, graphql } from "gatsby"
 
-// Styles
-import { Container } from "../styles/GlobalStyles"
+// // Styles
+// import { Container } from "../styles/GlobalStyles"
 
-const ContactInfo = () => {
-  return (
-    <StaticQuery
-      query={contactQuery}
-      render={data => {
-        const { email, phone } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-        return (
-          <Container>
-            <div className="contact">
-              <div>{email}</div>
-              <div>{phone}</div>
-            </div>
-          </Container>
-        )
-      }}
-    />
-  )
-}
+// const ContactInfo = () => {
+//   return (
+//     <StaticQuery
+//       query={contactQuery}
+//       render={data => {
+//         const { email, phone } = data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+//         return (
+//           <Container>
+//             <div className="contact">
+//               <div>{email}</div>
+//               <div>{phone}</div>
+//             </div>
+//           </Container>
+//         )
+//       }}
+//     />
+//   )
+// }
 
 // const contactQuery = graphql`
 //   query ContactQuery {
@@ -40,4 +40,4 @@ const ContactInfo = () => {
 //   }
 // `
 
-export default ContactInfo
+// export default ContactInfo
